0acea198f64a0cd420ba4030bebff8be5aaebc00
barretorodrigo <barretorodrigo@gmail.com>
2020-01-28 14:52:44 -0300

    Adding O.O.

--- todo/dbconn.php
+++ todo/dbconn.php
@@ -1,14 +1,13 @@
 <?php
 
 $servername = "localhost";
-$username   = "";
-$password   = "";
+$username   = "barreto";
+$password   = "barreto.2";
 $dbname     = "todo";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-if (!$conn) {
-	die("Connection failed: ".mysqli_connect_error());
-}
 
-?>
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+	die("Connection failed: ".$conn->connect_error);
+}
--- todo/list.php
+++ todo/list.php
@@ -1,11 +1,12 @@
 <?php
 include 'dbconn.php';
-$sql    = "SELECT id, titulo, descricao, data, status FROM tarefa";
-$result = mysqli_query($conn, $sql);
+$sql    = "SELECT id, titulo, descricao, data, status FROM tarefas";
+$result = $conn->query($sql);
 
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -13,75 +14,78 @@ $result = mysqli_query($conn, $sql);
   <title>ToDo</title>
 
   <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 </head>
- <body>
+
+<body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">ToDo</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <a class="navbar-brand" href="#">ToDo</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="list.php">Lista</a>
-      </li>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="list.php">Lista</a>
+        </li>
 
-    </ul>
+      </ul>
 
+    </div>
+  </nav>
+
+  <div class="container">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Título</th>
+          <th scope="col">Descrição</th>
+          <th scope="col">Data</th>
+          <th scope="col">Ação</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        <?php
+
+        if ($result->num_rows > 0) {
+          while ($row = $result->fetch_assoc()) { ?>
+            <tr>
+              <td scope="col"> <?php echo $row["id"]; ?></td>
+              <td scope="col"><?php echo $row["titulo"]; ?></td>
+              <td scope="col"><?php echo $row["descricao"]; ?></td>
+              <td scope="col"><?php echo $row["data"]; ?></td>
+              <td scope="col"><?php
+                              if ($row["status"]) {
+                                echo "<span class='badge badge-pill badge-primary'>Tarefa concluída</span>";
+                              } else {
+                                echo "<a href='update.php?id=" . $row['id'] . "'><button type='button' class='btn btn-success'>Concluir</button></a>";
+                              }
+
+                              ?></td>
+            </tr>
+
+        <?php }
+        } else {
+          echo "0 results";
+        }
+        ?>
+
+      </tbody>
+    </table>
   </div>
-</nav>
 
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Título</th>
-      <th scope="col">Descrição</th>
-      <th scope="col">Data</th>
-      <th scope="col">Ação</th>
-    </tr>
-  </thead>
-  <tbody>
-
-<?php
-
-if (mysqli_num_rows($result) > 0) {
-	while ($row = mysqli_fetch_assoc($result)) {?>
-																			<tr>
-																			      <td scope="col"> <?php echo $row["id"];?></td>
-																			      <td scope="col"><?php echo $row["titulo"];?></td>
-																			      <td scope="col"><?php echo $row["descricao"];?></td>
-																			      <td scope="col"><?php echo $row["data"];?></td>
-																			      <td scope="col"><?php
-		if ($row["status"]) {
-			echo "<span class='badge badge-pill badge-primary'>Tarefa concluída</span>";
-		} else {
-			echo "<a href='update.php?id=".$row['id']."'><button type='button' class='btn btn-success'>Concluir</button></a>";
-		}
-
-		?></td>
-																			</tr>
-
-		<?php }
-} else {
-	echo "0 results";
-}
-?>
-
-  </tbody>
-</table>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
 </html>
